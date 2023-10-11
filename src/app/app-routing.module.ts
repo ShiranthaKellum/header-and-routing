f6396c0177b2f58aca5632e07228bacc90e2a7f0
Shirantha Jayasooriya <skellumjayasooriya@gmail.com>
2023-10-11 15:32:08 +0530

    First commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPatientComponent } from './add-patient/add-patient.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AddPatientComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
