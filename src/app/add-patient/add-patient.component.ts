8103c5e0616ca00b4289a181c8d54a6a9dd14fe0
Shirantha Jayasooriya <skellumjayasooriya@gmail.com>
2023-10-17 01:12:05 +0530

    Add patient form.

--- src/app/add-patient/add-patient.component.ts
+++ src/app/add-patient/add-patient.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { PatientService } from '../services/patient.service';
+import { Patient } from '../model/patient';
 
 @Component({
   selector: 'app-add-patient',
   templateUrl: './add-patient.component.html',
-  styleUrls: ['./add-patient.component.css']
+  styleUrls: ['./add-patient.component.css'],
 })
 export class AddPatientComponent {
-
+  constructor(private patientService: PatientService) {}
+  submitted: boolean = false;
+  patient: Patient = {
+    name: '',
+    age: '',
+    gender: '',
+    country: '',
+    height: '',
+    weight: '',
+    heartRate: '',
+    bloodPressure: '',
+    sugar: '',
+    allergies: '',
+    symptoms: '',
+    diseases: '',
+    treatments: '',
+    medicines: '',
+    history: '',
+    observations: '',
+  };
+  addPatient(): void {
+    const data = {
+      name: this.patient.name,
+      age: this.patient.age,
+      gender: this.patient.gender,
+      country: this.patient.country,
+      height: this.patient.height,
+      weight: this.patient.weight,
+      heartRate: this.patient.heartRate,
+      bloodPressure: this.patient.bloodPressure,
+      sugar: this.patient.sugar,
+      allergies: this.patient.allergies,
+      symptoms: this.patient.symptoms,
+      diseases: this.patient.diseases,
+      treatments: this.patient.treatments,
+      medicines: this.patient.medicines,
+      history: this.patient.history,
+      observations: this.patient.observations
+    }
+    this.patientService.create(data).subscribe({
+      next: res => {
+        console.log("Submission responce: ", res);
+        this.submitted = true;
+      },
+      error: e => console.log("Submission error: ", e)
+    })
+  }
 }
---
+++ src/app/model/patient.ts
@@ -0,0 +1,18 @@
+export class Patient {
+  name?: string;
+  age?: string;
+  gender?: string;
+  country?: string;
+  height?: string;
+  weight?: string;
+  heartRate?: string;
+  bloodPressure?: string;
+  sugar?: string;
+  allergies?: string;
+  symptoms?: string;
+  diseases?: string;
+  treatments?: string;
+  medicines?: string;
+  history?: string;
+  observations?: string;
+}
